613083fced966a3fc2a1ca4d3f3685b80c7567f5
Anthony Prudhomme <anthony@mbp-de-anthony.home>
2017-02-23 11:15:51 +0100

    Updated app.js

--- Projet_Web_Design/js/app.js
+++ Projet_Web_Design/js/app.js
@@ -29,61 +29,26 @@ app.controller('pageBController',[
 ]);
 
 app.controller('pageAController',[
-	'$http',
-	function($http){
-		console.log("lol");
-		this.test1="du texte";
-
+	'$http','$scope',
+	function($http,$scope){
 		var controller = this;
-		var vgRatesLoaded = false;
-		var vgSalesLoaded = false;
-
-		this.allDatas = [];
-		
-
-		this.vgRates = [];
-		$http.get('datas/vg_rates.json').success(
+		this.vgDatas = [];
+		$http.get('datas/video_games_datas.json').success(
 			function(result){
-				controller.vgRates = result;
-				vgRatesLoaded = true;
-				console.log("Yo2");
-				mixDatas();
-			}
-		);
-		this.vgSales = [];
-		console.log("lol 2");
-		$http.get('datas/vg_sales.json').success(
-			function(result){
-				controller.vgSales = result;
-				vgSalesLoaded = true;
-				console.log("Yo");
-				mixDatas();
+				controller.vgDatas = result;
+				console.log(controller.vgRates);
 			}
 		);
 
-		function mixDatas(){
-			console.log(controller.vgRates);
-			if(vgSalesLoaded && vgRatesLoaded){
-				console.log(controller.vgSales.length);
-				//for (var i = 0; i < controller.vgRates.length; i++) {
-					//for (var j = 0; j < controller.vgSales.length; j++) {
-				for (var i = 0; i < 500; i++) {
-					for (var j = 0; j < 500; j++) {
-						if(controller.vgSales[i].Name.indexOf(controller.vgRates[i].FIELD3) !== -1){
-							console.log("ici:" + i+ " "+controller.vgSales[i].Name + " "+ controller.vgRates[i].FIELD3);
-							controller.allDatas.push(controller.vgSales[i]);
-							//controller.allDatas[i].rates = controller.vgRates[i];
-						}
-					}
-					//console.log(i);
-				}
-				console.log(controller.allDatas);
-				console.log("done");
-			}else{
-				console.log("Not loaded yet");
-			}
-		}; 
-		
+		$scope.logos = [{src:"img/logo_ps3.png",class:"logo-unselected"},{src:"img/logo_xone.png",class:"logo-unselected"}];
+	  	$scope.changeClass = function(index){
+	  		console.log($scope.logos);
+
+		    if ($scope.logos[index].class === "logo-unselected")
+		      $scope.logos[index].class = "logo-selected";
+		    else
+		      $scope.logos[index].class = "logo-unselected";
+	  	};
 	}
 ]);
 
@@ -113,4 +78,33 @@ app.filter('reverse',function(){
 		return output_values;
 	}
 });
+
+app.filter('consoleFilter',function(){
+	return function(input_values,toUpperCase){
+		if (typeof(input_values) == "string") {
+			var output_values="";
+			for(var i=0;i<input_values.length;i++){
+				output_values = input_values.charAt(i)+output_values;
+			}
+			if (toUpperCase) {
+				output_values=output_values.toUpperCase();
+			}
+		}else{
+			if(typeof(input_values) != "undefined"){
+				if(typeof(input_values) == "object"){
+					var output_values=[];
+					for(var i=input_values.length-5;i>= 0;i--){
+						output_values.push(input_values[i]);
+					}
+				}
+			}else{
+				throw("You apply this filter to an undefined object");
+			}
+		}
+		
+		return output_values;
+	}
+});
+
+
 
