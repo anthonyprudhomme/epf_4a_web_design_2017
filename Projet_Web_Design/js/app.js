f8701c51fcb27899fea256503289d3ef7e4a5db2
Yiouki <hugo.ruiz@epfedu.fr>
2017-02-23 17:34:28 +0100

    Update of app.js

--- Projet_Web_Design/js/app.js
+++ Projet_Web_Design/js/app.js
@@ -2,6 +2,8 @@ var app = angular.module('store',[
 	'ngRoute'
 ]);
 
+selectedConsoles = [];
+
 angular.module('store').config([
 	'$routeProvider',
 	function($routeProvider){
@@ -40,14 +42,21 @@ app.controller('pageAController',[
 			}
 		);
 
-		$scope.logos = [{src:"img/logo_ps3.png",class:"logo-unselected"},{src:"img/logo_xone.png",class:"logo-unselected"}];
+		$scope.logos = [{src:"img/logo_ps3.png",class:"logo-unselected",name:"PlayStation 3"},{src:"img/logo_xone.png",class:"logo-unselected",name:"Xbox One"}];
+	  	
 	  	$scope.changeClass = function(index){
 	  		console.log($scope.logos);
-
-		    if ($scope.logos[index].class === "logo-unselected")
-		      $scope.logos[index].class = "logo-selected";
-		    else
-		      $scope.logos[index].class = "logo-unselected";
+		    if ($scope.logos[index].class === "logo-unselected"){
+		      	$scope.logos[index].class = "logo-selected";
+		  		selectedConsoles.push($scope.logos[index]);
+		  	}
+		    else{
+		      	$scope.logos[index].class = "logo-unselected";
+		      	var itemIndex = selectedConsoles.indexOf($scope.logos[index]);
+		  		if (itemIndex > -1) {
+				    selectedConsoles.splice(itemIndex, 1);
+				}
+		  	}
 	  	};
 	}
 ]);
@@ -66,7 +75,7 @@ app.filter('reverse',function(){
 			if(typeof(input_values) != "undefined"){
 				if(typeof(input_values) == "object"){
 					var output_values=[];
-					for(var i=input_values.length;i> 0;i--){
+					for(var i=input_values.length-1;i>= 0;i--){
 						output_values.push(input_values[i]);
 					}
 				}
@@ -80,31 +89,27 @@ app.filter('reverse',function(){
 });
 
 app.filter('consoleFilter',function(){
-	return function(input_values,toUpperCase){
-		if (typeof(input_values) == "string") {
-			var output_values="";
-			for(var i=0;i<input_values.length;i++){
-				output_values = input_values.charAt(i)+output_values;
-			}
-			if (toUpperCase) {
-				output_values=output_values.toUpperCase();
-			}
-		}else{
-			if(typeof(input_values) != "undefined"){
-				if(typeof(input_values) == "object"){
-					var output_values=[];
-					for(var i=input_values.length-5;i>= 0;i--){
+	return function(input_values,$scope){
+		console.log(selectedConsoles);
+		if(typeof(input_values) != "undefined"){
+			if(typeof(input_values) == "object"){
+				var output_values=[];
+				for(var i=0;i < input_values.length;i++){
+					if(selectedConsoles.length != 0){
+					for (var j = 0; j< selectedConsoles.length; j++) {
+						if( input_values[i].Platform.indexOf(selectedConsoles[j].name) !== -1){
+							output_values.push(input_values[i]);
+						}
+					}
+					}else{
 						output_values.push(input_values[i]);
 					}
 				}
-			}else{
-				throw("You apply this filter to an undefined object");
 			}
+		}else{
+			throw("You apply this filter to an undefined object");
 		}
 		
 		return output_values;
 	}
 });
-
-
-
