71095204a0cef6e4215d620cf3a9a93144147968
Anthony Prudhomme <anthony@MacBook-Pro-de-Anthony.local>
2017-03-01 16:57:19 +0100

    Made chart.js and angular-chart.js working

--- Projet_Web_Design/js/app.js
+++ Projet_Web_Design/js/app.js
@@ -1,5 +1,5 @@
 var app = angular.module('store',[
-	'ngRoute'
+	'ngRoute','chart.js'
 ]);
 
 selectedConsoles = [];
@@ -66,6 +66,38 @@ app.controller('pageAController',[
 	}
 ]);
 
+app.controller("LineCtrl", function ($scope) {
+
+  $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+  $scope.series = ['Series A', 'Series B'];
+  $scope.data = [
+    [65, 59, 80, 81, 56, 55, 40],
+    [28, 48, 40, 19, 86, 27, 90]
+  ];
+  $scope.onClick = function (points, evt) {
+    console.log(points, evt);
+  };
+  $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
+  $scope.options = {
+    scales: {
+      yAxes: [
+        {
+          id: 'y-axis-1',
+          type: 'linear',
+          display: true,
+          position: 'left'
+        },
+        {
+          id: 'y-axis-2',
+          type: 'linear',
+          display: true,
+          position: 'right'
+        }
+      ]
+    }
+  };
+});
+
 app.filter('reverse',function(){
 	return function(input_values,toUpperCase){
 		if (typeof(input_values) == "string") {
@@ -119,3 +151,5 @@ app.filter('consoleFilter',function(){
 		return output_values;
 	}
 });
+
+
