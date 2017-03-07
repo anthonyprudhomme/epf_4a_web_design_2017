786e6a0eecdbbb3e502a8e52ba0095099a524bbd
Yiouki <hugo.ruiz@epfedu.fr>
2017-03-07 23:19:50 +0100

    End of the beginning of files

--- Projet_Web_Design/js/app.js
+++ Projet_Web_Design/js/app.js
@@ -1,19 +1,19 @@
 /*
-==================================================================================================================================
-|© Copyright de l'entreprise HARP-e fondée par Anthony Prudhomme & Hugo Ruiz
-|
-|"java.js" est l'un des fichiers les plus importants du site internet.
-|
-|C'est lui qui permet de charger la BDD (Base De Données) et de les classer dans des tableaux afin de pouvoir
-|effectuer des opérations. Il permet aussi de charger la page HTML pour créer le lien entre les 2 fichiers.
-|
-|Le fichier est structuré de la manière suivante :
-|	1. Les variables globales
-|	2. Modules
-|	3. Les contrôleurs
-|	4. Fonctions
-|	5. Filtres
-==================================================================================================================================
+===============================================================================================================
+|© Copyright de l'entreprise HARP-e fondée par Anthony Prudhomme & Hugo Ruiz								  |
+|                                                                                                             |
+|"java.js" est l'un des fichiers les plus importants du site internet.										  |
+|                                                                                                             |
+|C'est lui qui permet de charger la BDD (Base De Données) et de les classer dans des tableaux afin de pouvoir |
+|effectuer des opérations. Il permet aussi de charger la page HTML pour créer le lien entre les 2 fichiers.	  |
+|                                                                                                             |
+|Le fichier est structuré de la manière suivante key: "value", 												  |
+|	1. Les variables globales 																				  |
+|	2. Modules																								  |
+|	3. Les contrôleurs																						  |
+|	4. Fonctions																							  |
+|	5. Filtres 																								  |
+===============================================================================================================
 */
 
 var app = angular.module('videoGames',[
